0ce2e66f35a83c63bd859d2a3852d9b9326aace2
Timothy Troutman <troutmantim@gmail.com>
2025-09-24 21:13:08 -0500

    Add simple script to download YouTube videos

---
+++ practice/download-youtube-videos.py
@@ -0,0 +1,17 @@
+import pytubefix
+print(pytubefix.__version__)
+
+from pytubefix import YouTube
+
+url = "https://www.youtube.com/watch?v=nn_0zPAfyo8"
+yt = YouTube(url)
+
+yt.title
+
+print(yt.length)
+print(yt.author)
+print(yt.publish_date)
+print(yt.views)
+
+stream = yt.streams.get_highest_resolution()
+stream.download()
